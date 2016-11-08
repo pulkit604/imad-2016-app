6e314067b21e5d181e382a98e95473bbd2c4342d
pulkit604 <guppul@gmail.com>
2016-11-09 01:26:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/dp.jpg', function (req, res) {
+app.get('/ui/images/dp.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'dp.jpg'));
 });
 
