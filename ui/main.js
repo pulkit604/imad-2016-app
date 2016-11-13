f6e54613eede81aa5b8402ee7e841c580dc4508b
pulkit604 <guppul@gmail.com>
2016-11-13 20:15:00 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -14,24 +14,23 @@ console.log('Loaded!');
         request.onreadystatechange = function() {
           if(reuest.readyState === XMLHttpRequest.DONE) {
               if(request.status ===200){
-                  
-                  var names = request.responseText;
-                  names = JSON.parse(names);
-                  var list = '';
-                  for (var i=0; i< names.length; i++){
-                    
-                      list+= '<li>' +names[i] + '</li>';
-                  }
-                
-                var ul = document.getElementbyId('namelist');
-                ul.innerHTML = list;
-                } 
-                      };
+                  alert('Login successful');
+              }else if(request.status === 403){
+                    alert('Username or password not correct');
+              }else if(request.status === 500){
+                    alert('Server error');
+              }
+          }
+        };
+            
+             
         
       
-      var nameInput = document.getElementbyId('name');
+      var username = document.getElementbyId('username').value;
+      var password = document.getElementbyId('password').value;
         var name = nameInput.value;
-          request.open('GET', 'http://pukit604.imad.hasura-app.io/submit-name?name=' +name, true);
-          request.send(null);
+          request.open('POST', 'https://pukit604.imad.hasura-app.io/login', true);
+            request.setRequestHeader('Content-Type', 'application/json');
+          request.send(JSON.stringify({username:username, password:password}));
         };  
       
