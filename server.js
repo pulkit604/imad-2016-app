23ea52feda1d216210dbc93bff817c624338d50a
pulkit604 <guppul@gmail.com>
2016-11-13 18:11:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,12 +13,33 @@ var config = {
 };
 
 
+var pool = new Pool(config);
+
+
+
+
+
 var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+
+function hash(input,salt){
+  
+ var hashed = crypto.pbkdf2Sync(input,salt,15000,256,'sha512');
+  return ["pbkdf2","15000",salt,hashed.toString('hex')].join('$');
+  
+}
+
+
+
+
+
+
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
