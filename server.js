30b01e911c300afae61a3edcf95c9bbbd462f5de
pulkit604 <guppul@gmail.com>
2016-11-18 04:24:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -118,6 +118,9 @@ app.get('/logout', function(req,res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 
 app.get('/ui/images/dp.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui/images', 'dp.jpg'));
