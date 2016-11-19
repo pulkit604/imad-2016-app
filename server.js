98a4fef3fc5d005423e588c481b432438ab87196
pulkit604 <guppul@gmail.com>
2016-11-19 06:53:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var session = require('express-session');
 var config = {
   user : 'pulkit604',
   database : 'pulkit604',
-  host : 'localhost',
+  host : 'db.imad.hasura-app,io',
   port : '5432',
   password : process.env.DB_PASSWORD
 };
