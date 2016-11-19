7e57ae844fcaf9b57682d018213c1b98b665cd4f
pulkit604 <guppul@gmail.com>
2016-11-19 07:34:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,7 +22,7 @@
       
       var username = document.getElementById('username').value;
       var password = document.getElementById('password').value;
-          request.open('POST', 'https://pukit604.imad.hasura-app.io/login', true);
+          request.open('POST', '/login', true);
             request.setRequestHeader('Content-Type', 'application/json');
           request.send(JSON.stringify({username:username, password:password}));
           
