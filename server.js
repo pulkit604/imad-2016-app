a747261bb4c5c26ef06fb1d412cc02cac36908c8
pulkit604 <guppul@gmail.com>
2016-11-19 07:34:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -49,8 +49,8 @@ function hash(input,salt){
 
 app.get('/create-user', function (req,res){
   
-    var username = document.getElementById('username');
-    var password = document.getElementById('password');
+    var username =req.body.username;
+    var password = req.body.password;
   
     var salt = crypto.getRandomBytes(128).toString('hex');
     var dbString = hash(password,salt);
